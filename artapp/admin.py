a7a083840a9625169aa4e0c9be76a0eb33bcdb36
shr33sha <shreesharout@gmail.com>
2024-11-06 14:16:26 +0530

    Remove artapp directory from the repository

--- artapp/admin.py
+++
@@ -1,6 +0,0 @@
-# artapp/admin.py
-from django.contrib import admin
-from .models import Artist, Category
-
-admin.site.register(Artist)
-admin.site.register(Category)
--- artapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArtappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'artapp'
